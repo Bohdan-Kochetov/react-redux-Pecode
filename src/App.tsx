bb9182f736921b28971a13b08195ead41403562f
bohdan_kochetov <bodja1208@gmail.com>
2021-11-08 15:08:11 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { productFetchData, selected, chooseProductId } from './store/reducer';
-import { ProductDetalis } from './ProductDetalis';
-import { FormToAdd } from './FormToAdd';
+import React from 'react';
+import { } from './store/reducer';
 import './App.scss';
-import { State } from './react-app-env';
 
 const App: React.FC = () => {
-  const dispatch = useDispatch();
-  const {
-    productToRender, select, productId, add,
-  } = useSelector((state: State) => state.reducer);
-
-  useEffect(() => {
-    dispatch(productFetchData());
-  }, [select]);
-
   return (
-    <div className="App">
-      <h1>List of products</h1>
-      Sort by:
-      {' '}
-      <select
-        value={select}
-        onChange={event => {
-          dispatch(selected(event.target.value));
-          dispatch({
-            type: event.target.value,
-          });
-        }}
-      >
-        <option value="select">Choose</option>
-        <option value="alphabetically">Alphabetically</option>
-        <option value="count">Count</option>
-      </select>
-      <ul className="list-group">
-        {productToRender.map((item: any) => {
-          return (
-            <>
-              <li key={item.id} className="list-group-item">
-                <h2 className="h2">{item.name}</h2>
-                <button
-                  className="btn btn-info"
-                  type="button"
-                  onClick={() => dispatch(chooseProductId(item.id))}
-                >
-                  Show details
-
-                </button>
-                <button
-                  className="btn btn-danger"
-                  type="button"
-                  onClick={() => dispatch({
-                    type: 'delete',
-                    id: item.id,
-                  })}
-                >
-                  Delete
-
-                </button>
-              </li>
-            </>
-          );
-        })}
-      </ul>
-      <div>
-        <button
-          className="btn btn-success"
-          type="button"
-          onClick={() => dispatch({
-            type: 'add',
-            add: true,
-          })}
-        >
-          Add Product
-
-        </button>
-      </div>
-
-      {add && (
-        <div>
-          <h2>Give me some informarion, and then I can add a product  :)</h2>
-          <FormToAdd />
-        </div>
-      )}
-      {productId && (
-        <ProductDetalis />
-      )}
-    </div>
+    <div className="App" />
   );
 };
 
 export default App;
-// qqq
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,12 +1,7 @@
-import { request } from '../apiGoods';
-import { Product } from '../react-app-env';
+import { request } from '../api';
 
 const initialState = {
-  product: [],
-  productToRender: [],
-  select: 'select',
-  productId: 0,
-  add: false,
+
 };
 
 export function productSuccess(product: any) {
@@ -21,38 +16,6 @@ export function productFetchData() {
     .then(result => dispatch(productSuccess(result)));
 }
 
-export function selected(string: string) {
-  return string === 'count' ? {
-    type: 'filterByCount',
-  } : {
-    type: 'filterByAlphabet',
-  };
-}
-
-export function chooseProductId(id: number) {
-  return {
-    type: 'choosenProduct',
-    productId: id,
-  };
-}
-
-function generateProduct(length: number, arr: any) {
-  const [name, imageUrl, count, weight, width, height] = arr;
-
-  return {
-    id: length + 1,
-    name,
-    imageUrl,
-    count,
-    size: {
-      width,
-      height,
-    },
-    weight: `${weight}g`,
-    comments: [],
-  };
-}
-
 export function reducer(state = initialState, action: any) {
   switch (action.type) {
     case 'productSuccess':
@@ -60,54 +23,6 @@ export function reducer(state = initialState, action: any) {
         ...state,
         product: action.product,
         productToRender: action.product,
-      };
-    case 'filterByCount':
-      return {
-        ...state,
-        productToRender: state.product.sort((a: Product, b: Product) => a.count - b.count),
-      };
-    case 'filterByAlphabet':
-      return {
-        ...state,
-        productToRender: state.product
-          .sort((a: Product, b: Product) => a.name.localeCompare(b.name)),
-      };
-    case 'alphabetically':
-      return {
-        ...state,
-        select: 'alphabetically',
-      };
-    case 'count':
-      return {
-        ...state,
-        select: 'count',
-      };
-    case 'select':
-      return {
-        ...state,
-        select: 'select',
-      };
-    case 'choosenProduct':
-      return {
-        ...state,
-        productId: action.productId,
-      };
-    case 'add':
-      return {
-        ...state,
-        add: !state.add,
-      };
-    case 'delete':
-      return {
-        ...state,
-        product: state.product.filter((item: Product) => item.id !== action.id),
-        productToRender: state.product.filter((item: Product) => item.id !== action.id),
-      };
-    case 'addProduct':
-      return {
-        ...state,
-        product: [...state.product, generateProduct(state.product.length, action.details)],
-        productToRender: [...state.product, generateProduct(state.product.length, action.details)],
       };
     default:
       return state;
